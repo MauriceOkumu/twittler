cf69ce0eac1f34603a3861cdee8ef23ca54a9e5a
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2016-11-25 17:20:29 -0800

    Started work on twittler, buttons working

---
+++ buttons.js
@@ -0,0 +1,56 @@
+
+// var stream = streams.home;
+//       var $w = $('<div>www</div>');
+
+//       var getTweet = function(name){
+//         var t = []
+//         for(var i = 0; i < stream.length; i++){
+//           console.log(stream[i].user)
+//           if(stream[i].user === name){
+//             t.push( '@'+name+''+stream[i].message);
+//           }
+//         }
+//         for(var i = t.length-1; i>=0;i--){
+//           var $news = $('<div id="how">how</div>')
+//           $news.text(t[i])
+//           console.log(t[i])
+//           $news.appendTo($w);
+//         };
+
+
+//       };
+
+// var $body = $('body');
+// var shawn = $('<button id="shawn">shawndrost</button>')
+// shawn.appendTo($body);
+// $("#shawn").on("click",function(){
+//   $w.text('')
+//   console.log('clicked')
+//   getTweet('shawndrost')
+// });
+// var sharksforcheap = $('<button id="sharksforcheap">sharksforcheap</button>')
+// sharksforcheap.appendTo($body);
+// $("#sharksforcheap").on("click",function(){
+//   $w.text('')
+//   getTweet('sharksforcheap')
+// });
+// var mracus = $('<button id="mracus"><span>mracus</span></button>')
+// mracus.appendTo($body);
+// $("#mracus").on("click",function(){
+//   $w.text('')
+//   getTweet('mracus')
+// });
+// var douglascalhoun = $('<button id="douglascalhoun">douglascalhoun</button>')
+// douglascalhoun.appendTo($body);
+// $("#douglascalhoun").on("click",function(){
+//   $w.text('')
+//   getTweet('douglascalhoun')
+// });
+// //$w.appendTo($body);
+
+
+// console.log('hey')
+
+
+
+
--- data_generator.js
+++ data_generator.js
@@ -27,7 +27,7 @@ var randomElement = function(array){
 };
 
 // random tweet generator
-var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
+var opening = ['Maurice','just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
 var verbs = ['drank', 'drunk', 'deployed', 'got', 'developed', 'built', 'invented', 'experienced', 'fought off', 'hardened', 'enjoyed', 'developed', 'consumed', 'debunked', 'drugged', 'doped', 'made', 'wrote', 'saw'];
 var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
 var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
@@ -58,6 +58,11 @@ scheduleNextTweet();
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
+var visit = function(msg){
+  this.msg = msg
+  return this.msg
+};
+var visitor = new visit('Hello')
 var writeTweet = function(message){
   if(!visitor){
     throw new Error('set the global visitor property!');
@@ -67,3 +72,5 @@ var writeTweet = function(message){
   tweet.message = message;
   addTweet(tweet);
 };
+// writeTweet(new visit('hello'))
+
